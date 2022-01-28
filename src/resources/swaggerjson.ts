e3ef9f1258ed2fb95db7898c9712e10a41c9a19c
Felipe Cabrera <felipe@bylearn.com.br>
2022-01-28 06:00:32 -0300

    ESLint

--- src/resources/swaggerjson.ts
+++ src/resources/swaggerjson.ts
@@ -1,8 +1,8 @@
-const swagger = require('./swagger');
+const swagger = require("./swagger")
 
 exports.handler = async () => {
-    return {
-        statusCode: 200,
-        body: JSON.stringify(swagger),
-    };
-};
+  return {
+    statusCode: 200,
+    body: JSON.stringify(swagger),
+  }
+}
