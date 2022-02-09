d51a61a7aee2d6325b99bd3a144361ee3e01f695
David Wright <david.wright@bayer.com>
2022-02-09 13:46:59 -0600

    update to swagger-ui 4.5.0

--- src/resources/swagger-html.py
+++ src/resources/swagger-html.py
@@ -1,29 +1,38 @@
 swagger_ui = """<!DOCTYPE html>
-<html>
-<link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@3.12.1/swagger-ui.css">
-
-<head>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <title>SwaggerUI</title>
+    <link
+      rel="stylesheet"
+      href="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui.css"
+    />
+  </head>
+  <body>
     <div id="swagger-ui"></div>
-    <script src="https://unpkg.com/swagger-ui-dist@3.12.1/swagger-ui-bundle.js"></script>
-    <script src="https://unpkg.com/swagger-ui-dist@3.12.1/swagger-ui-standalone-preset.js"></script>
+    <script
+      src="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-bundle.js"
+      crossorigin
+    ></script>
+    <script
+      src="https://unpkg.com/swagger-ui-dist@4.5.0/swagger-ui-standalone-preset.js"
+      crossorigin
+    ></script>
     <script>
-       window.onload = function() {
-           const ui = SwaggerUIBundle({
-              url: window.location.href + ".json",
-              dom_id: '#swagger-ui',
-              presets: [
-                SwaggerUIBundle.presets.apis,
-                SwaggerUIStandalonePreset
-                ],
-              layout: "StandaloneLayout"
-           })
-           window.ui = ui;
-       }
+      window.onload = () => {
+        const ui = SwaggerUIBundle({
+          url: window.location.href + '.json',
+          dom_id: '#swagger-ui',
+          presets: [SwaggerUIBundle.presets.apis, SwaggerUIStandalonePreset],
+          layout: 'StandaloneLayout',
+        });
+        window.ui = ui;
+      };
     </script>
-</head>
-
-<body></body>
+  </body>
 </html>
+
 """
 
 
